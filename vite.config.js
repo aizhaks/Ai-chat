950e63796049ae666763624210924710867abd70
aizhaks <aidana.zhks@gmail.com>
2025-07-10 12:45:26 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcssVite from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcssVite()],
+  plugins: [react()],
 })
